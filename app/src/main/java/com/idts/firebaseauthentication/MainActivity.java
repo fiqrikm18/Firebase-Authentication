f23dd39d79c3a7821381f57e1cb03aeef546a085
fiqri khoirul m <fiqrikm18@gmail.com>
2019-10-31 12:12:36 +0700

    initial commit

---
+++ app/src/main/java/com/idts/firebaseauthentication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.idts.firebaseauthentication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+    }
+}
